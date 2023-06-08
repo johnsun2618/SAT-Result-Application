66722b4fec9c2d358d560c40aea61a8ae5e17b0d
Anmol das <johnsun2618@gmail.com>
2023-06-08 12:56:04 +0530

    SAT Result Application

--- src/main/java/com/example/SAT_Results/Controller/SATResultController.java
+++ src/main/java/com/example/SAT_Results/Controller/SATResultController.java
@@ -21,35 +21,43 @@ public class SATResultController {
 
 //    1. Insert data - First API
     @PostMapping
-    public SATResult insertSATResult(@RequestBody SATResult satResult) {
+    public SATResult insertSATResult(@RequestBody SATResult satResult)
+    {
+
         return satResultServiceImplmentation.insertSATResult(satResult);
+
     }
 
 
 
 //    2. View all data - Second API
     @GetMapping
-    public List<SATResult> getAllSATResults() {
+    public List<SATResult> getAllSATResults()
+    {
+
         return satResultServiceImplmentation.getAllSATResults();
+
     }
 
 
 
 //    3. Get rank - Third API
     @GetMapping("/{name}")
-    public ResponseEntity<SATResult> getRankByName(@PathVariable String name) {
+    public ResponseEntity<SATResult> getRankByName(@PathVariable String name)
+    {
+
         Optional<SATResult> optionalSATResult = satResultServiceImplmentation.getRankByName(name);
         return optionalSATResult.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
     }
 
 
 
 //    4. Update score - Forth API
     @PutMapping("/{name}")
-    public ResponseEntity<SATResult> updateSATScoreByName(
-            @PathVariable String name,
-            @RequestParam int satScore
-    ) {
+    public ResponseEntity<SATResult> updateSATScoreByName(@PathVariable String name,
+                                                          @RequestParam int satScore)
+    {
         Optional<SATResult> optionalSATResult = satResultServiceImplmentation.updateSATScoreByName(name, satScore);
         return optionalSATResult.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
@@ -58,12 +66,15 @@ public class SATResultController {
 
 //    5. Delete one record - Fifth API
     @DeleteMapping("/{name}")
-    public ResponseEntity<String> deleteSATResultByName(@PathVariable String name) {
+    public ResponseEntity<String> deleteSATResultByName(@PathVariable String name)
+    {
+
         boolean deleted = satResultServiceImplmentation.deleteSATResultByName(name);
         if (deleted) {
             return ResponseEntity.ok("Record deleted successfully");
         } else {
             return ResponseEntity.notFound().build();
         }
+
     }
 }
--- src/main/java/com/example/SAT_Results/Service/Implimentation/SATResultServiceImplmentation.java
+++ src/main/java/com/example/SAT_Results/Service/Implimentation/SATResultServiceImplmentation.java
@@ -16,24 +16,31 @@ public class SATResultServiceImplmentation implements SATResultService {
     SATResultRepository satResultRepository;
 
     @Override
-    public SATResult insertSATResult(SATResult satResult) {
+    public SATResult insertSATResult(SATResult satResult)
+    {
+
         int satScore = satResult.getSatScore();
         satResult.setPassed(satScore > 30 ? "Pass" : "Fail");
         return satResultRepository.save(satResult);
+
     }
 
     @Override
-    public List<SATResult> getAllSATResults() {
+    public List<SATResult> getAllSATResults()
+    {
         return satResultRepository.findAll();
     }
 
     @Override
-    public Optional<SATResult> getRankByName(String name) {
+    public Optional<SATResult> getRankByName(String name)
+    {
         return satResultRepository.findByName(name);
     }
 
     @Override
-    public Optional<SATResult> updateSATScoreByName(String name, int satScore) {
+    public Optional<SATResult> updateSATScoreByName(String name, int satScore)
+    {
+
         Optional<SATResult> optionalSATResult = satResultRepository.findByName(name);
         optionalSATResult.ifPresent(satResult -> {
             satResult.setSatScore(satScore);
@@ -41,17 +48,21 @@ public class SATResultServiceImplmentation implements SATResultService {
             satResultRepository.save(satResult);
         });
         return optionalSATResult;
+
     }
 
 
     @Override
-    public boolean deleteSATResultByName(String name) {
+    public boolean deleteSATResultByName(String name)
+    {
+
         Optional<SATResult> optionalSATResult = satResultRepository.findByName(name);
         if (optionalSATResult.isPresent()) {
             satResultRepository.delete(optionalSATResult.get());
             return true;
         }
         return false;
+
     }
 
 
